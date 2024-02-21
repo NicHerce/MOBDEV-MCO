ad958e2139ae983824dda3a76b5fa9319d0979be
Dominic Marcus Reyes Herce <102027083+NicHerce@users.noreply.github.com>
2024-02-21 09:53:25 +0800

    Base File.
    November. Indigo. Golf. Golf. Alpha.
    Over.

---
+++ app/src/main/java/com/mobdeve/s12/group9/mobdev_mco/MainActivity.kt
@@ -0,0 +1,15 @@
+/*
+*
+* */
+
+package com.mobdeve.s12.group9.mobdev_mco
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
