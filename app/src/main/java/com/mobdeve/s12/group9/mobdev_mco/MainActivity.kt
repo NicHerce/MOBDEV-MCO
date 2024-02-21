c40ca6c284b563169de07fb524060d3b1b491797
SeanHigginsLim <sean_higgins_lim@dlsu.edu.ph>
2024-02-21 11:31:20 +0800

    try

--- app/src/main/java/com/mobdeve/s12/group9/mobdev_mco/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s12/group9/mobdev_mco/MainActivity.kt
@@ -6,11 +6,13 @@ package com.mobdeve.s12.group9.mobdev_mco
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewbinding.ViewBinding
+import com.mobdeve.s12.group9.mobdev_mco.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        yuh
-        setContentView(R.layout.activity_main)
+        val viewBinding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
     }
 }
