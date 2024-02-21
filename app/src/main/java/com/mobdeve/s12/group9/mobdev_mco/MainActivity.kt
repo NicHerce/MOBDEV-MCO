14b04a3e04c842112460f7b3fca2a88a3f6d24f3
SeanHigginsLim <sean_higgins_lim@dlsu.edu.ph>
2024-02-21 10:41:24 +0800

    try

--- app/src/main/java/com/mobdeve/s12/group9/mobdev_mco/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s12/group9/mobdev_mco/MainActivity.kt
@@ -10,6 +10,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        yuh
         setContentView(R.layout.activity_main)
     }
 }
