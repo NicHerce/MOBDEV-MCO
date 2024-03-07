933d33a3ce1a913d51717d1bbd285a3f0bf3e205
Dominic Marcus Reyes Herce <102027083+NicHerce@users.noreply.github.com>
2024-03-07 16:03:40 +0800

    added locations, profile, and edit profile activities. need to implement activity deletion for device back button onclick.

--- app/src/main/java/com/mobdeve/s12/group9/mobdev_mco/HomeActivity.kt
+++ app/src/main/java/com/mobdeve/s12/group9/mobdev_mco/HomeActivity.kt
@@ -1,5 +1,6 @@
 package com.mobdeve.s12.group9.mobdev_mco
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -17,7 +18,7 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Initialize ViewBinding for the ProfileActivity
+        // Initialize ViewBinding for the HomeActivity
         val viewBinding: ActivityHomeBinding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
 
@@ -26,5 +27,10 @@ class HomeActivity : AppCompatActivity() {
         this.myAdapter = MyAdapter(locationList)
         this.recyclerView.adapter = myAdapter
         recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+
+        viewBinding.profileBtn.setOnClickListener {
+            val intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/mobdeve/s12/group9/mobdev_mco/ProfileActivity.kt
+++ app/src/main/java/com/mobdeve/s12/group9/mobdev_mco/ProfileActivity.kt
@@ -1,5 +1,6 @@
 package com.mobdeve.s12.group9.mobdev_mco
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mobdeve.s12.group9.mobdev_mco.databinding.ActivityMainBinding
@@ -12,5 +13,17 @@ class ProfileActivity : AppCompatActivity() {
         // Initialize ViewBinding for the ProfileActivity
         val viewBinding: ActivityProfileBinding = ActivityProfileBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
+
+        // Edit Account Button
+        viewBinding.btnEditAccount.setOnClickListener {
+            val intent = Intent(this, EditProfileActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Locations Button
+        viewBinding.locationsBtn.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
